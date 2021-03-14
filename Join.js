1bc3e0c3a75297bb5b6490df333ab78a31d6ef12
vlyly <S2oonae@gmail.com>
2021-03-14 22:14:00 +0900

    test commit

--- Join.js
+++ Join.js
@@ -323,9 +323,18 @@ function search_address() {
 
       // 우편번호와 주소 정보를 해당 필드에 넣는다.
       address_postcode_input.value = data.zonecode;
-      address_detail_input.value = addr;
+      address_input.value = addr;
       // 커서를 상세주소 필드로 이동한다.
       address_detail_input.value.focus();
+    },
+    onclose: function (state) {
+      //state는 우편번호 찾기 화면이 어떻게 닫혔는지에 대한 상태 변수 이며, 상세 설명은 아래 목록에서 확인하실 수 있습니다.
+      if (state === "FORCE_CLOSE") {
+        //사용자가 브라우저 닫기 버튼을 통해 팝업창을 닫았을 경우, 실행될 코드를 작성하는 부분입니다.
+      } else if (state === "COMPLETE_CLOSE") {
+        //사용자가 검색결과를 선택하여 팝업창이 닫혔을 경우, 실행될 코드를 작성하는 부분입니다.
+        //oncomplete 콜백 함수가 실행 완료된 후에 실행됩니다.
+      }
     },
   }).open();
 }
